fd955ef4142c016728b54ba6b2c25b41fcc7d0b7
Love Beling <love.beling@se.experis.com>
2022-03-08 13:44:21 +0100

    feat(UserController): implemented get users endpoint

--- src/main/java/com/example/timebankapiproject/controller/UserController.java
+++ src/main/java/com/example/timebankapiproject/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.example.timebankapiproject.controller;
 
+import com.example.timebankapiproject.models.UserModel;
+import com.example.timebankapiproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
 public class UserController {
+
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserModel>> getUsers(){
+
+        return ResponseEntity.ok().body(userRepository.findAll());
+    }
 }
