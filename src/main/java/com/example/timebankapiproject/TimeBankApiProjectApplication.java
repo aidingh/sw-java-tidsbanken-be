ecf59be12044a4573fa539a98e559f9d8527d2d2
aidingh <aidingh@hotmail.com>
2022-03-08 09:01:57 +0100

    FEAT: First backend project commit.

---
+++ src/main/java/com/example/timebankapiproject/TimeBankApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.timebankapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBankApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeBankApiProjectApplication.class, args);
+    }
+
+}
