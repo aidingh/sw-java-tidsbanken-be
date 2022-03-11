3698cc0d3eef53d2f82328c252859fa809f60e85
Love Beling <love.beling@se.experis.com>
2022-03-11 10:28:01 +0100

    feat(UserController): added method for fetching bearer token. Added logic for creating a user on the auth0 server side

--- src/main/java/com/example/timebankapiproject/controller/UserController.java
+++ src/main/java/com/example/timebankapiproject/controller/UserController.java
@@ -3,9 +3,15 @@ package com.example.timebankapiproject.controller;
 import com.example.timebankapiproject.models.UserModel;
 import com.example.timebankapiproject.models.VacationRequestModel;
 import com.example.timebankapiproject.service.UserService;
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -30,9 +36,48 @@ public class UserController {
         return userService.getUserById(userId);
     }
 
+    public String getManagementApiToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("client_id", "gcpFQMIuEMTPdur0XhbRekUKWLSsLF3K");
+        requestBody.put("client_secret", "RTEExKEXK603fBA11Y4s22IsaBV95PE3YvvK3A6fVwa-_ms16Gp9JHvmjLQiq0dN");
+        requestBody.put("audience", "https://dev-377qri7m.eu.auth0.com/api/v2/");
+        requestBody.put("grant_type", "client_credentials");
+
+        HttpEntity<String> request = new HttpEntity<String>(requestBody.toString(), headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HashMap<String, String> result = restTemplate
+                .postForObject("https://dev-377qri7m.eu.auth0.com/oauth/token", request, HashMap.class);
+
+        return result.get("access_token");
+    }
+
     @PostMapping("/user")
-    public ResponseEntity <UserModel> createUser(@RequestBody UserModel userModel) {
-        return userService.createUser(userModel);
+    public ResponseEntity <String> createUser() {
+        JSONObject request = new JSONObject();
+        request.put("email", "norman.lewis@email.com");
+        request.put("given_name", "Norman");
+        request.put("family_name", "Lewis");
+        request.put("connection", "Username-Password-Authentication");
+        request.put("password", "hej/23vad&och%");
+
+
+        //userService.createUser(userModel);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        System.out.println(getManagementApiToken());
+        headers.set("authorization", "Bearer " + getManagementApiToken());
+
+        HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate
+                .postForEntity("https://dev-377qri7m.eu.auth0.com/api/v2/users", entity, String.class);
+
+        return result;
     }
     @PatchMapping("/user/")
     public ResponseEntity <UserModel> updateUser(@RequestBody UserModel userModel) {
