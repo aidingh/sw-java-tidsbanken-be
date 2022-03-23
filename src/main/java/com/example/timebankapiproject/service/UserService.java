6cec88a2bce7cda8c9d04feb459ab62b3fb6e28e
Love Beling <love.beling@se.experis.com>
2022-03-23 11:41:22 +0100

    docs(UserService): comments on methods

--- src/main/java/com/example/timebankapiproject/service/UserService.java
+++ src/main/java/com/example/timebankapiproject/service/UserService.java
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 
 @Service
 public class UserService {
-
-
     private final UserRepository userRepository;
     private final VacationRequestRepository vacationRequestRepository;
 
@@ -22,6 +20,10 @@ public class UserService {
         this.vacationRequestRepository = vacationRequestRepository;
     }
 
+    /**
+     * Finds all users.
+     * @return Response entity with all users
+     */
     public ResponseEntity <List<UserModel>> getAllUsers (){
         List<UserModel> users = userRepository.findAll();
         if(!users.isEmpty())
@@ -30,6 +32,11 @@ public class UserService {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Finds a specific user by id.
+     * @param userId
+     * @return Response entity with found user or Response entity with status code Not found.
+     */
     public ResponseEntity <UserModel> getUserById(String userId){
 
         UserModel user;
@@ -41,11 +48,21 @@ public class UserService {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Creates a user in the database.
+     * @param userModel User to be created.
+     */
     public void createUserInDatabase(UserModel userModel){
         if(userModel != null){
            userRepository.save(userModel);
         }
     }
+
+    /**
+     * Finds a user by id and returns the user.
+     * @param id
+     * @return Returns found user.
+     */
     public UserModel findUserById(String id){
         Optional<UserModel> user = userRepository.findById(id);
         if(user.isPresent()){
@@ -56,10 +73,19 @@ public class UserService {
         }
     }
 
+    /**
+     * Saves a user to the database.
+     * @param user
+     */
     public void saveUser(UserModel user){
         userRepository.save(user);
     }
 
+    /**
+     * Deletes a user from the database by id.
+     * @param userId
+     * @return
+     */
     public boolean deleteUser(String userId){
         if(userRepository.existsById(userId)){
            userRepository.deleteById(userId);
@@ -68,6 +94,11 @@ public class UserService {
            return false;
     }
 
+    /**
+     * Get all vacationRequests of a user.
+     * @param userId Id of the user.
+     * @return ResponseEntity with all vacationrequests in the body.
+     */
     public ResponseEntity<List<VacationRequestModel>> getUserVacationRequestsById(String userId){
 
         List <VacationRequestModel> allVacRequests;
